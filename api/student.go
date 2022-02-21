b2eee21064cb381bb7246d478b5357f46ee69c9c
JasonLaw <luozuxuan@foxmail.com>
2022-02-21 10:56:56 +0800

    update

---
+++ api/student.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"Project/service"
+	"Project/vo"
+
+	"github.com/gin-gonic/gin"
+)
+
+var studentService service.StudentService
+
+//抢课
+func BookCourse(c *gin.Context) {
+	var courseVo vo.BookCourseRequest
+	var res vo.BookCourseResponse
+
+	if err := c.ShouldBind(&courseVo); err == nil {
+		res = studentService.BookCourse(&courseVo)
+		c.JSON(200, res)
+	} else {
+		res.Code = vo.UnknownError
+		c.JSON(200, res)
+	}
+}
+
+//查询学生课表
+func GetStudentCourse(c *gin.Context) {
+	var courseVo vo.GetStudentCourseRequest
+	res := vo.GetStudentCourseResponse{}
+	if err := c.ShouldBind(&courseVo); err == nil {
+		res = studentService.GetStudentCourse(&courseVo)
+		c.JSON(200, res)
+	} else {
+		res.Code = vo.UnknownError
+		c.JSON(200, res)
+	}
+}
---
+++ conf/conf.go
@@ -0,0 +1,24 @@
+package conf
+
+import (
+	"Project/cache"
+	"Project/common/util"
+	"Project/model"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// Init 初始化配置项
+func Init() {
+	// 从本地读取环境变量
+	godotenv.Load()
+
+	// 设置日志级别
+	util.BuildLogger(os.Getenv("LOG_LEVEL"))
+
+	// 连接数据库
+	model.Database(os.Getenv("MYSQL_DSN"))
+	cache.Redis()
+
+}
